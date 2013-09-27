b546b8708bbed10552eab39061699a1a5d333869
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-27 23:09:56 +0200

    parser draft

---
+++ h/parse_task_system_stream.h
@@ -0,0 +1,50 @@
+#ifndef OS_PARSE_TASK_SYSTEM_STREAM_H
+#define OS_PARSE_TASK_SYSTEM_STREAM_H
+
+namespace os{
+	template<typename I, typename S>
+	void parse_task_system_stream(I& stream, S& system){
+		char pt;
+		size_t i = 0;
+		bool skip = true;
+		size_t current[4] = {};
+		while(true){
+			stream.get(pt);
+			if(!stream.good()) break;
+			switch(pt){
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+				case '0':{
+					skip = false;
+					current[i] *= 10;
+					current[i] += (pt - '0');
+				}
+
+				case ' ' :
+				case '\t':
+				case '\n':{
+					if(!skip){
+						++i;
+						if(i == 4){
+							system.add(current);
+							i = 0;
+							skip = true;
+							current = {};
+						}
+					}
+				}
+			}
+		}
+		if(!skip) system.add(current);
+	}
+}
+
+
+#endif // OS_PARSE_TASK_SYSTEM_STREAM_H
