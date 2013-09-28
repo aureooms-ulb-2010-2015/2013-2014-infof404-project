590134f4b0c8befa3566095a21137855e445938b
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-28 11:18:32 +0200

    test print task_t

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "os/task_t.h"
+#include "lib/io.h"
 
 int main(){
 	std::cout << "Hello world!" << std::endl;
+	os::task_t task;
+	std::cout << task << std::endl;
 	return 0;
 }
