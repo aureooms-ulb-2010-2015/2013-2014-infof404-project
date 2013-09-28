163c19efdf5ded46483e88bd793d82d8421686ac
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-28 11:27:48 +0200

    std::ifstream test

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
+#include <fstream>
+
 #include "os/task_t.h"
 #include "os/task_system_t.h"
 #include "lib/io.h"
 
 int main(){
 	std::cout << "Hello world!" << std::endl;
-	
+
 	{
-	os::task_t task;
-	std::cout << task << std::endl;
+		os::task_t task;
+		std::cout << task << std::endl;
 	}
 
 	{
-	os::task_system_t task_system(5);
-	std::cout << task_system << std::endl;
+		os::task_system_t task_system(5);
+		std::cout << task_system << std::endl;
+	}
+
+	{
+		std::ifstream ifs("system/0", std::ifstream::in);
+		char c = ifs.get();
+		while (ifs.good()){
+			std::cout << c;
+			c = ifs.get();
+		}
+		ifs.close();
 	}
 	return 0;
 }
