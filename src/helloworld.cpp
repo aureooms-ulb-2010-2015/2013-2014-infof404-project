77a05841ad3767e5cd447a1617af274ae30f8c73
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-28 11:21:30 +0200

    clean

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -5,11 +5,15 @@
 
 int main(){
 	std::cout << "Hello world!" << std::endl;
-
+	
+	{
 	os::task_t task;
 	std::cout << task << std::endl;
+	}
 
+	{
 	os::task_system_t task_system(5);
 	std::cout << task_system << std::endl;
+	}
 	return 0;
 }
