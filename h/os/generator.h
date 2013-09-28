94de328070311a4d6e2cbf4c53ba515d584f6322
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-28 13:08:40 +0200

    include generator (no syntax error) b

---
+++ h/os/generator.h
@@ -0,0 +1,32 @@
+#ifndef OS_GENERATOR_H
+#define OS_GENERATOR_H
+
+#include <algorithm>
+
+namespace os{
+	template<typename G, typename D,typename U, typename N, typename S>
+	void generate_task_system(G& generator, D& distribution, const U usage, const N n, S& task_system){
+		if(n > 0){
+			U left = usage;
+			N i = 0;
+			while(i < n-1){
+				U u = distribution(generator) % left + 1 - (n-i);
+				left -= u;
+				uint offset = 0;
+				uint period = 100;
+				uint deadline = u + distribution(generator) % (period - u + 1);
+				uint wcet = u;
+				task_system.emplace_back(offset, period, deadline, wcet);
+				++i;
+			}
+			uint offset = 0;
+			uint period = 100;
+			uint deadline = left + distribution(generator) % (period - left + 1);
+			uint wcet = left;
+			task_system.emplace_back(offset, period, deadline, wcet);
+		}
+	}
+}
+
+
+#endif // OS_GENERATOR_H
