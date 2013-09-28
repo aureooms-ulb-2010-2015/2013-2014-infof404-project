6785e6231c21cce0ce43ddd43dc9e5e138720d49
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-28 11:20:05 +0200

    test task_system_t print

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "os/task_t.h"
+#include "os/task_system_t.h"
 #include "lib/io.h"
 
 int main(){
 	std::cout << "Hello world!" << std::endl;
+
 	os::task_t task;
 	std::cout << task << std::endl;
+
+	os::task_system_t task_system(5);
+	std::cout << task_system << std::endl;
 	return 0;
 }
