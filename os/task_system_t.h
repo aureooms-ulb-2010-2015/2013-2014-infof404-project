22683aee9483cfc4acc20705fb0f0137a02dde4f
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-09-28 10:59:06 +0200

    data structures drafts

---
+++ os/task_system_t.h
@@ -0,0 +1,12 @@
+#ifndef OS_TASK_SYSTEM_T_H
+#define OS_TASK_SYSTEM_T_H
+
+#include "os/task_t.h"
+#include <vector>
+
+namespace os{
+	typedef std::vector<task_t> task_system_t;
+}
+
+
+#endif // OS_TASK_SYSTEM_T_H
---
+++ os/task_t.h
@@ -0,0 +1,14 @@
+#ifndef OS_TASK_T_H
+#define OS_TASK_T_H
+
+namespace os{
+	typedef struct task_t{
+		uint offset
+		uint period
+		uint deadline
+		uint wcet
+	} task_t;
+}
+
+
+#endif // OS_TASK_T_H
