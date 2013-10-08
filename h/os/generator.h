3f33586db7273974e821ceb9deeffb4707578a8c
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-10-08 23:07:12 +0200

    filled gen/wiwcdb/limits

--- h/os/generator.h
+++ h/os/generator.h
@@ -2,80 +2,22 @@
 #define OS_GENERATOR_H
 
 #include <algorithm>
+#include <set>
 
 namespace os{
 	template<typename G, typename D,typename U, typename N, typename S>
 	void generate_task_system(G& generator, D& distribution, const U usage, const N n, S& task_system){
 		if(n > 0){
-			U left = usage;
-			N i = 0;
-			while(i < n-1){
-				U u = distribution(generator) % (left - n + i) + 1;
-				left -= u;
-				uint offset = 0;
-				uint period = 100;
-				uint deadline = u + distribution(generator) % (period - u + 1);
-				uint wcet = u;
-				task_system.emplace_back(offset, period, deadline, wcet);
-				++i;
+			double sep[n+1] = {};
+			for(N i = 1; i < n; ++i){
+				sep[i] = rate_distribution(generator);
 			}
-			uint offset = 0;
-			uint period = 100;
-			uint deadline = left + distribution(generator) % (period - left + 1);
-			uint wcet = left;
-			task_system.emplace_back(offset, period, deadline, wcet);
-		}
-	}
-
-	template<typename G, typename D,typename U, typename N, typename S>
-	void generate_task_system2(G& generator, D& distribution, const U usage, const N n, S& task_system){
-		if(n > 0){
-			U left = usage;
-			N i = 0;
-			D period_distribution(std::max(1 + (n * 100 - 1) / usage, distribution.min()),distribution.max());
-			while(i < n-1){
-				U p = period_distribution(generator);
-				U u = distribution(generator) % (left - n + i) + 1;
-				left -= u;
+			for(N i = 0; i < n; ++i){
 				uint offset = 0;
-				uint period = p;
-				uint deadline = u + distribution(generator) % (distribution.max() - u + 1);
-				uint wcet = 1 + (u * p - 1) / distribution.max();
-				task_system.emplace_back(offset, period, deadline, wcet);
-				++i;
+				uint period = period_distribution(generator);
+				uint wcet = (sep[i+1] - sep[i])*period;
+				uint deadline = wcet + period_distribution(generator) % (period - wcet + 1);
 			}
-			U p = period_distribution(generator);
-			uint offset = 0;
-			uint period = p;
-			uint deadline = left + distribution(generator) % (distribution.max() - left + 1);
-			uint wcet = 1 + (left * p - 1) / distribution.max();
-			task_system.emplace_back(offset, period, deadline, wcet);
-		}
-	}
-
-	template<typename G, typename D, typename N, typename S>
-	void generate_task_system2(G& generator, D& distribution, const double usage, const N n, S& task_system){
-		if(n > 0){
-			double left = usage;
-			N i = 0;
-			while(i < n-1){
-				uint r = 1.0 / left;
-				double p = distribution(generator) % (distribution.max() - r) + r;
-				double u = distribution(generator) % (p - n + i) + 1;
-				uint offset = 0;
-				uint period = p;
-				uint wcet = u * p / distribution.max();
-				uint deadline = wcet + distribution(generator) % (distribution.max() - u + 1);
-				task_system.emplace_back(offset, period, deadline, wcet);
-				++i;
-				left -= (double)wcet/(double)period;
-			}
-			double p = period_distribution(generator);
-			uint offset = 0;
-			uint period = p;
-			uint deadline = left + distribution(generator) % (distribution.max() - left + 1);
-			uint wcet = 1 + (left * p - 1) / distribution.max();
-			task_system.emplace_back(offset, period, deadline, wcet);
 		}
 	}
 
