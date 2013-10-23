7edf96d0deab49a4e823eaf19922e16344cd9d2e
OOMS  Aurelien <aurelien.ooms@gmail.com>
2013-10-23 13:42:48 +0200

    plot mean working with args

--- src/plot_mean.cpp
+++ src/plot_mean.cpp
@@ -1,38 +1,171 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <string>
+#include <fstream>
 
 #include "os/io.h"
 #include "os/study/plot.h"
 #include "lib/pinput.h"
 #include "lib/exception.h"
 
-int main(int argc, char const *argv[]){
+template<typename O, typename S, typename C>
+void fill_parameters(const O& options, S& file_name_in, bool& open_in, S& file_name_out, C& color_good, C& color_bad, C& color_axis, double& stroke_width, double& res, double& x_res, double& y_res, double& scale_res){
+
+	typename O::const_iterator it;
+
+	if((it = options.find("-o")) != options.end() || (it = options.find("--output")) != options.end()){
+		if(it->second.size() > 0){
+			file_name_out = it->second[0];
+		}
+	} else throw lib::exception("Cannot find -o option");
+
+	if((it = options.find("-c")) != options.end() || (it = options.find("--color")) != options.end()){
+		if(it->second.size() > 5){
+			color_good.r = std::stoi(it->second[0]);
+			color_good.g = std::stoi(it->second[1]);
+			color_good.b = std::stoi(it->second[2]);
+			color_bad.r = std::stoi(it->second[3]);
+			color_bad.g = std::stoi(it->second[4]);
+			color_bad.b = std::stoi(it->second[5]);
+		}
+	}
+
+	if((it = options.find("-i")) != options.end() || (it = options.find("--input")) != options.end()){
+		if(it->second.size() > 0){
+			file_name_in = it->second[0];
+			open_in = true;
+		}
+	}
+
+
+	if((it = options.find("-r")) != options.end() || (it = options.find("--res")) != options.end()){
+		if(it->second.size() > 0){
+			res = x_res = y_res = std::stod(it->second[0]);
+			if(it->second.size() > 1){
+				y_res = std::stod(it->second[1]);
+			}
+		}
+	}
+
+	if((it = options.find("--sr")) != options.end() || (it = options.find("--scaleres")) != options.end()){
+		if(it->second.size() > 0){
+			scale_res = std::stod(it->second[0]);
+		}
+	}
+
+	if((it = options.find("-a")) != options.end() || (it = options.find("--axiscolor")) != options.end()){
+		if(it->second.size() > 0){
+			color_axis.r = color_axis.g = color_axis.b = std::stoi(it->second[0]);
+			if(it->second.size() > 2){
+				color_axis.g = std::stoi(it->second[1]);
+				color_axis.b = std::stoi(it->second[2]);
+			}
+		}
+	}
+
+	if((it = options.find("-s")) != options.end() || (it = options.find("--strokewidth")) != options.end()){
+		if(it->second.size() > 0){
+			stroke_width = std::stod(it->second[0]);
+		}
+	}
+
+}
+
+void help(){
+	std::cout << " - flags" << std::endl << std::endl;
+	std::cout << "   " << "[-h | --help]" << std::endl;
+	std::cout << "   " << "[-v | --verbose]" << std::endl << std::endl;
+	std::cout << " - mandatory parameters" << std::endl << std::endl;
+	std::cout << "   " << "[-u | --utilization] ... (double[] | v[i] >= 0)" << std::endl;
+	std::cout << "   " << "[-d | --delta      ] ... (uint[])" << std::endl;
+	std::cout << "   " << "-n                   ... (uint[])" << std::endl;
+	std::cout << "   " << "-k                   ... (uint)" << std::endl << std::endl;
+	std::cout << " - optional parameters" << std::endl << std::endl;
+	std::cout << "   " << "[-p | --period] #0 [#1] (int[2], #1 >= #0)" << std::endl;
+	std::cout << "   " << "[-s | --seed  ] #0 (uint)" << std::endl;
+	std::cout << "   " << "[-o | --output] #0 (string)" << std::endl << std::endl;
+	std::cout << "   " << "[-m | --mode  ] #0 (string)" << std::endl << std::endl;
+}
+
+int main(int argc, char *argv[]){
 	try{
-		std::cout << "SVG TEST" << std::endl;
-		
+
+
+		std::vector<std::string> params;
+		std::map<std::string, std::vector<std::string>> options;
+		std::set<std::string> flags;
+		std::set<std::string> option_set = {
+			"-o", "--output",
+			"-c", "--color",
+			"-i", "--input",
+			"-r", "--res",
+			"-sr", "--scaleres",
+			"-a", "--axiscolor",
+			"-s", "--strokewidth",
+		};
+		std::set<std::string> flag_set = {
+			"-h", "--help",
+			"-v", "--verbose"
+		};
+
+		pinput::parse(argc, argv, params, options, flags, option_set, flag_set);
+
+		if(flags.count("-h") || flags.count("--help")){
+			help();
+			return 0;
+		}
+
+		std::string file_name_in;
+		bool open_in = false;
+		std::string file_name_out;
+
+
+		double res = 50, x_res = 50, y_res = 50, scale_res = 30, stroke_width = 1;
+		lib::color color_axis(0, 0, 0), color_good(255, 255, 255), color_bad(0, 0, 0);
+
+		fill_parameters(options, file_name_in, open_in, file_name_out, color_good, color_bad, color_axis, stroke_width, res, x_res, y_res, scale_res);
+
+		// CHOOSE INPUT
+
+		std::streambuf* buffer;
+		std::ifstream ifstream;
+
+		if(open_in){
+			ifstream.open(file_name_in);
+			buffer = ifstream.rdbuf();
+		}
+		else{
+			buffer = std::cin.rdbuf();
+		}
+		std::istream istream(buffer);
+
+
+		// LOAD DATA
 		size_t u_width, d_width;
 
 		std::vector<std::vector<double>> mean;
 		std::vector<double> vector_u;
 		std::vector<uint> vector_d;
+		
+		os::load_mean(istream, mean, vector_u, vector_d, u_width, d_width);
+		if(open_in) ifstream.close();
 
-		os::load_mean(std::cin, mean, vector_u, vector_d, u_width, d_width);
 
+		//PLOT DATA
 		double boundaries[2] = {0, 1};
 
-		const double res = 50, x_res = 50, y_res = 50, scale_res = 30;
-		const svg::Color axis_color(0, 0, 0);
-		const svg::Stroke axis_stroke(1, axis_color);
-		const std::string file = "svg/3.svg";
-		const lib::color color_good(255, 255, 255), color_bad(0, 0, 0);
-
+		const svg::Color axis_color(color_axis.r, color_axis.g, color_axis.b);
+		const svg::Stroke axis_stroke(stroke_width, axis_color);
 
 		svg::Dimensions dimensions(150 + u_width * x_res, 150 + d_width * y_res);
 		svg::Layout layout(dimensions, svg::Layout::BottomLeft);
 
-		svg::Document doc(file, layout);
+		svg::Document doc(file_name_out, layout);
 		os::study::plot_mean(doc, mean, boundaries[0], boundaries[1], u_width, d_width, res, color_good, color_bad, 75, 75);
 		os::study::plot_scale(doc, 0, 1, 0.2, scale_res, color_good, color_bad, 0, 0);
-		os::study::plot_axis(doc, "u", vector_u, u_width, x_res, "d", vector_d, d_width, y_res, axis_stroke, axis_color, 74, 74);
+		os::study::plot_axis(doc, "u", vector_u, u_width, x_res, "d", vector_d, d_width, y_res, axis_stroke, axis_color, 75, 75);
 		doc.save();
 
 		std::cout << std::endl;
