f31f8f767a73faecf2e34b8283845616fde1ed58
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-11-04 13:49:43 +0100

    fix/clean atkin

--- 2/src/Atkin.cpp
+++ 2/src/Atkin.cpp
@@ -9,8 +9,7 @@
 
 int main(int argc, char const *argv[]){
 	
-	if (argc<2)
-	{
+	if (argc<2){
 		std::cout<<"N?"<<std::endl;
 		return 1;
 	}
@@ -19,52 +18,43 @@ int main(int argc, char const *argv[]){
 
 	begin = clock();
 
-	int n_max=std::stoull(argv[1]);
+	size_t n_max = std::stoull(argv[1]);
 
 
 	std::vector<uint64_t> prime;
-	bool sieve[n_max];	
-	memset(sieve, 0, sizeof(sieve));
+	std::vector<bool> sieve(n_max + 1, false);
+
 
 	prime.push_back(2);
 	prime.push_back(3);
 	prime.push_back(5);
 
-	sieve[2]=sieve[3]=sieve[5]=true;
+	sieve[2] = sieve[3] = sieve[5] = true;
 
-	int N = sqrt(n_max);
-	for (int x = 1; x <= N ; ++x)
-	{
-		for (int y = 1; y <= N; ++y)
-		{
-			int n = (4*x*x)+(y*y);
-			if ((n <= n_max) && ((n % 12 == 1) or (n % 12 == 5)))
-			{
-				sieve[n] ^= true;//Xor with true toggle bool
-			}
+	size_t N = sqrt(n_max);
+	for(size_t x = 1; x <= N ; ++x){
+		for(size_t y = 1; y <= N; ++y){
+
+			size_t n = (4*x*x)+(y*y);
+			if ((n <= n_max) && ((n % 12 == 1) || (n % 12 == 5))) sieve[n] = sieve[n] xor true; //Xor with true toggle bool
+
 			n = (3*x*x)+(y*y);
-			if ( (n <=n_max) && (n % 12 == 7 ))
-			{
-				sieve[n] ^= true;
-			}
+			if ((n <= n_max) && (n % 12 == 7)) sieve[n] = sieve[n] xor true;
+
 			n = (3*x*x)-(y*y);
-			if ((x>y) && (n<=n_max) && (n %12==11))
-			{
-				sieve[n] ^= true;
-			}
+			if ((x > y) && (n <= n_max) && (n % 12 == 11)) sieve[n] = sieve[n] xor true;
 		}
 	}
 	
-	int a;
-   for (a = 7; a <= N; a+=2){
-      if (sieve[a]){
-         for (int i = a*a; i < n_max; i += a*a)
-            sieve[i] = false;
-         prime.push_back(a);
-      }
-   }
-   for (; a < n_max; a+=2) if (sieve[a])
-      prime.push_back(a);
+	size_t a;
+	for(a = 7; a <= N; a += 2){
+		if(sieve[a]){
+			for (size_t i = a*a; i <= n_max; i += a*a) sieve[i] = false;
+			prime.push_back(a);
+		}
+	}
+
+	for(; a <= n_max; a += 2) if(sieve[a]) prime.push_back(a);
 
 	end = clock();
 	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
