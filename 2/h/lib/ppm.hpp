1551457a7b635f6562be3c35c29794ad2f226b87
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-11-15 20:18:37 +0100

    add ppm output to test-eratosthene-fast

--- 2/h/lib/ppm.hpp
+++ 2/h/lib/ppm.hpp
@@ -5,11 +5,11 @@ namespace ppm{
 
 
 	typedef struct pixel_t{
-		uint8_t r;
-		uint8_t g;
-		uint8_t b;
+		int r;
+		int g;
+		int b;
 
-		pixel_t(const uint8_t r, const uint8_t g, const uint8_t b) : r(r), g(g), b(b) {}
+		pixel_t(const int r, const int g, const int b) : r(r), g(g), b(b) {}
 
 	} pixel_t;
 
@@ -22,7 +22,8 @@ namespace ppm{
 
 	template<typename S>
 	S& operator<<(S& out, const pixel_t& c){
-		return out << c.r << c.g << c.b;
+		if(out.flags() & std::ios::binary) return out << c.r << c.g << c.b;
+		else return out << c.r << ' ' << c.g << ' ' << c.b;
 	}
 
 	/**
@@ -44,7 +45,8 @@ namespace ppm{
 
 	template<typename S, typename A>
 	S& flush(S& out, const A& array, const size_t n){
-		for(size_t i = 0; i < n; ++i) out << array[i];
+		if(out.flags() & std::ios::binary) for(size_t i = 0; i < n; ++i) out << array[i];
+		else for(size_t i = 0; i < n; ++i) out << array[i] << ' ';
 		return out;
 	}
 
@@ -66,7 +68,7 @@ namespace ppm{
 	 *
 	 */
 	template<typename S>
-	S& write_header(S& out, const uint8_t magic, const size_t width, const size_t height, const uint16_t max, const char sep = '\20'){
+	S& write_header(S& out, const int magic, const size_t width, const size_t height, const uint16_t max, const char sep = ' '){
 		return out << 'P' << magic << sep << width << sep << height << sep << max << sep;
 	}
 
@@ -76,7 +78,7 @@ namespace ppm{
 	 *
 	 */
 	template<typename S>
-	S& read_header(S& in, uint8_t& magic, size_t& width, size_t& height, uint16_t& max){
+	S& read_header(S& in, int& magic, size_t& width, size_t& height, uint16_t& max){
 		in.get();
 		return in >> magic >> width >> height >> max;
 	}
--- 2/src/test-eratosthene-fast.cpp
+++ 2/src/test-eratosthene-fast.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 #include "lib/eratosthene.hpp"
 #include "lib/prime.hpp"
 #include "lib/ppm.hpp"
+#include "lib/ulam.hpp"
 
 int main(int argc, char const *argv[]){
 	if(argc != 2){
@@ -14,35 +16,57 @@ int main(int argc, char const *argv[]){
 
 	const size_t nth = std::stoull(argv[1]);
 	const size_t last = prime::upper_bound(nth);
-	std::cout << last << std::endl;
+	size_t nth_val = 0;
+	
+	std::vector<bool> prime;
 
 	if(nth < 1) return 0;
-	if(nth == 1){
-		std::cout << 2 << std::endl;
-		return 0;
+	else if(nth == 1){
+		nth_val = 2;
+		prime.resize(1, true);
+	}
+	else{
+		const size_t count = last / 2 + last % 2 - 1;
+
+		prime.resize(count, true);
+
+		size_t k = 3;
+		while(k * k < last){
+			const size_t i = eratosthene::number_to_index(k * k);
+			if(prime[i]) eratosthene::go_through(i, k, count, prime);
+			k += 2;
+		}
+
+		size_t ith = 1;
+
+		for(size_t i = 0; i < count; ++i){
+			if(prime[i]){
+				++ith;
+				if(ith == nth){
+					nth_val =  eratosthene::index_to_number(i);
+					break;
+				}
+			}
+		}
 	}
 
-	const size_t count = last / 2 + last % 2 - 1;
+	const size_t size = std::sqrt(nth_val) + 1;
+	const size_t pixels = size * size;
 
-	std::vector<bool> prime(count, true);
-
-	size_t k = 3;
-	while(k * k < last){
-		const size_t i = eratosthene::number_to_index(k*k);
-		if(prime[i]) eratosthene::go_through(i, k, count, prime);
-		k += 2;
+	ppm::write_header(std::cout, 3, size, size, 1);
+	std::cout << std::endl;
+	for(size_t i = 0; i < pixels;){
+		size_t k = ulam::stol(i, size) + 1;
+		if(k == 1) std::cout << ppm::pixel_t(0, 0, 0) << ' ';
+		else if(k == 2) std::cout << ppm::pixel_t(1, 1, 1) << ' ';
+		else if(k % 2 == 0) std::cout << ppm::pixel_t(0, 0, 0) << ' ';
+		else if(prime[eratosthene::number_to_index(k)]) std::cout << ppm::pixel_t(1, 1, 1) << ' ';
+		else std::cout << ppm::pixel_t(0, 0, 0) << ' ';
+		++i;
+		if(i % size == 0) std::cout << std::endl;
 	}
 
-	// size_t ith = 1;
-	// for(size_t i = 0; i < count; ++i){
-	// 	if(prime[i]){
-	// 		++ith;
-	// 		if(ith == nth){
-	// 			std::cout << eratosthene::index_to_number(i) << std::endl;
-	// 			break;
-	// 		}
-	// 	}
-	// }
+
 
 	return 0;
 }
