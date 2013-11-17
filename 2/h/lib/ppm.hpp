19dad830efc9084815eae883e8bce3e48a032f24
Ooms Aurélien <aurelien.ooms@gmail.com>
2013-11-18 00:56:14 +0100

    stash it :o

--- 2/h/lib/ppm.hpp
+++ 2/h/lib/ppm.hpp
@@ -1,15 +1,18 @@
 #ifndef PPM_HPP
 #define PPM_HPP
 
+#include <mpi.h>
+#include <string>
+
 namespace ppm{
 
 
 	typedef struct pixel_t{
-		int r;
-		int g;
-		int b;
+		uint8_t r;
+		uint8_t g;
+		uint8_t b;
 
-		pixel_t(const int r, const int g, const int b) : r(r), g(g), b(b) {}
+		pixel_t(const uint8_t r, const uint8_t g, const uint8_t b) : r(r), g(g), b(b) {}
 
 	} pixel_t;
 
@@ -23,11 +26,24 @@ namespace ppm{
 	template<typename S>
 	S& operator<<(S& out, const pixel_t& c){
 		if(out.flags() & std::ios::binary) return out << c.r << c.g << c.b;
-		else return out << c.r << ' ' << c.g << ' ' << c.b;
+		else return out << (int)c.r << ' ' << (int)c.g << ' ' << (int)c.b;
 	}
 
 	/**
-	 * Function template to read a pixel from a ppm stream. (4)
+	 * Function template to write a pixel to a ppm binary file over MPI. (4)
+	 *
+	 *
+	 */
+
+	template<typename F, typename S>
+	void write(F& file, const pixel_t& c, S& status){
+		MPI_File_write(file, &c.r, 1, MPI_BYTE, &status);
+		MPI_File_write(file, &c.g, 1, MPI_BYTE, &status);
+		MPI_File_write(file, &c.b, 1, MPI_BYTE, &status);
+	}
+
+	/**
+	 * Function template to read a pixel from a ppm stream. (2)
 	 *
 	 *
 	 */
@@ -38,7 +54,7 @@ namespace ppm{
 	}
 
 	/**
-	 * Function template to flush an array to a ppm stream. (4)
+	 * Function template to flush an array to a ppm stream. (2)
 	 *
 	 *
 	 */
@@ -63,7 +79,7 @@ namespace ppm{
 	}
 
 	/**
-	 * Function template to write a ppm header to a ppm stream. (4)
+	 * Function template to write a ppm header to a ppm stream. (2)
 	 *
 	 *
 	 */
@@ -73,7 +89,40 @@ namespace ppm{
 	}
 
 	/**
-	 * Function template to read a ppm header from a ppm stream. (4)
+	 * Function template to write a ppm header to a ppm binary file over MPI. (2)
+	 *
+	 *
+	 */
+	template<typename F, typename S>
+	size_t write_header(F& file, const char magic, const size_t width, const size_t height, const uint16_t max, S& status, const char sep = ' '){
+		const char P = 'P';
+		std::string repr;
+		size_t offset = 6;
+
+		MPI_File_write(file, &P, 1, MPI_CHAR, &status);
+		MPI_File_write(file, &magic, 1, MPI_CHAR, &status);
+		MPI_File_write(file, &sep, 1, MPI_CHAR, &status);
+
+		repr = std::to_string(width);
+		offset += repr.size();
+		MPI_File_write(file, &repr.c_srt(), repr.size(), MPI_CHAR, &status);
+		MPI_File_write(file, &sep, 1, MPI_CHAR, &status);
+
+		repr = std::to_string(height);
+		offset += repr.size();
+		MPI_File_write(file, &repr.c_srt(), repr.size(), MPI_CHAR, &status);
+		MPI_File_write(file, &sep, 1, MPI_CHAR, &status);
+
+		repr = std::to_string(max);
+		offset += repr.size();
+		MPI_File_write(file, &repr.c_srt(), repr.size(), MPI_CHAR, &status);
+		MPI_File_write(file, &sep, 1, MPI_CHAR, &status);
+
+		return offset;
+	}
+
+	/**
+	 * Function template to read a ppm header from a ppm stream. (2)
 	 *
 	 *
 	 */
--- 2/src/test-mpi.cpp
+++ 2/src/test-mpi.cpp
@@ -11,12 +11,14 @@
 #include "lib/io.hpp"
 #include "lib/bits_t.hpp"
 
+const int SETUP = 0;
+
 int main (int argc, char *argv[]){
 	MPI_Init(&argc, &argv);
 
-	int rank, size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	int mpi_rank, mpi_size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
 
 	if(argc != 2){
@@ -27,38 +29,93 @@ int main (int argc, char *argv[]){
 	const size_t nth = std::stoull(argv[1]);
 	const size_t last = prime::upper_bound(nth);
 	size_t nth_val = 0;
-	std::string file_name = "ppm/mpi.ppm";
 
 
 	std::vector<bool> prime;
 
-	MPI_File file;
-	MPI_File_open(MPI_COMM_WORLD, (char *) file_name.c_str(), MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
-	MPI_Offset offset = sizeof(unsigned int) * rank;
-	MPI_Status status;
-
-	if(nth == 0); //nop
-	else if(nth == 1) nth_val = 2;
-	else if(nth == 2) nth_val = 3;
+	if(nth == 0) last = 0;
+	else if(nth == 1) last = 4;
+	else if(nth == 2) last = 4;
 	else{
 		size_t count = last / 6;
 
 		if(last % 6 <= 4) count = 2 * count + 1;
 		else count = 2 * count + 2;
 
-		prime.resize(count, true);
+		size_t r = count % mpi_size;
+		count /= mpi_size;
 
-		for(size_t k = 5, l = 2; k * k < last; l += 2) {
-			const size_t i = eratosthene::number_to_index_23_1(k * k);
+
+		prime.resize(count, true);
+		size_t k, l, o;
+		if(r > mpi_rank){
+			++count;
+			o = mpi_rank * count;
+		}
+		else{
+			o = r * (count + 1) + (mpi_rank - r) * count;
+		}
+		const bool left = 0, right = 1;
+		bool which;
+		int current = 0, tmp = 0;
+		if(mpi_rank > current){
+			do{
+				MPI_Bcast(&tmp, 1, MPI_INT, current, MPI_COMM_WORLD);
+				if(tmp > current){
+					current = tmp;
+					continue;
+				}
+				size_t i;
+
+				MPI_Bcast(&i, 1, MPI_UNSIGNED, current, MPI_COMM_WORLD);
+				MPI_Bcast(&k, 1, MPI_UNSIGNED, current, MPI_COMM_WORLD);
+				MPI_Bcast(&l, 1, MPI_UNSIGNED, current, MPI_COMM_WORLD);
+				MPI_Bcast(&which, 1, MPI_BOOL, current, MPI_COMM_WORLD);
+
+
+				i = 2 * k - [(o - i) % (2 * k)];
+
+				if(which == left){
+					eratosthene::go_through(i, 2 * k, count, prime);
+					eratosthene::go_through(i + k - l, 2 * k, count, prime);
+				}
+				else{
+					eratosthene::go_through(i, 2 * k, count, prime);
+					eratosthene::go_through(i + k + l, 2 * k, count, prime);
+				}
+			}
+			while(mpi_rank > current);
+
+			MPI_Recv(&k, 1, MPI_UNSIGNED, mpi_rank - 1, SETUP, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&l, 1, MPI_UNSIGNED, mpi_rank - 1, SETUP, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+		else{
+			k = 5, l = 2;
+		}
+
+		for(; k * k < last; l += 2) {
+			const size_t i = eratosthene::number_to_index_23_1(k * k) - o;
+			if(i >= count) break;
 			if(prime[i]){
+				MPI_Bcast(&mpi_rank, 1, MPI_INT, mpi_rank, MPI_COMM_WORLD);
+				MPI_Bcast(&i, 1, MPI_UNSIGNED, mpi_rank, MPI_COMM_WORLD);
+				MPI_Bcast(&k, 1, MPI_UNSIGNED, mpi_rank, MPI_COMM_WORLD);
+				MPI_Bcast(&l, 1, MPI_UNSIGNED, mpi_rank, MPI_COMM_WORLD);
+				MPI_Bcast(&left, 1, MPI_BOOL, mpi_rank, MPI_COMM_WORLD);
 				eratosthene::go_through(i, 2 * k, count, prime);
 				eratosthene::go_through(i + k - l, 2 * k, count, prime);
 			}
 
 			k += 2;
 			if(k * k >= last) break;
-			const size_t j = eratosthene::number_to_index_23_1(k * k);
+			const size_t j = eratosthene::number_to_index_23_1(k * k) - o;
+			if(j >= count) break;
 			if(prime[j]){
+				MPI_Bcast(&mpi_rank, 1, MPI_INT, mpi_rank, MPI_COMM_WORLD);
+				MPI_Bcast(&j, 1, MPI_UNSIGNED, mpi_rank, MPI_COMM_WORLD);
+				MPI_Bcast(&k, 1, MPI_UNSIGNED, mpi_rank, MPI_COMM_WORLD);
+				MPI_Bcast(&l, 1, MPI_UNSIGNED, mpi_rank, MPI_COMM_WORLD);
+				MPI_Bcast(&right, 1, MPI_BOOL, mpi_rank, MPI_COMM_WORLD);
 				eratosthene::go_through(j, 2 * k, count, prime);
 				eratosthene::go_through(j + k + l, 2 * k, count, prime);
 			}
@@ -66,47 +123,61 @@ int main (int argc, char *argv[]){
 			k += 4;
 		}
 
-		size_t ith = 2;
-
-		for(size_t i = 0; i < count; ++i){
-			if(prime[i]){
-				++ith;
-				if(ith == nth){
-					nth_val = eratosthene::index_to_number_23_0(i);
-					break;
-				}
-			}
-			if(i >= count) break;
-			++i;
-			if(prime[i]){
-				++ith;
-				if(ith == nth){
-					nth_val =  eratosthene::index_to_number_23_1(i);
-					break;
-				}
-			}
+		if(mpi_rank + 1 < mpi_size){
+			MPI_Send(&k, 1, MPI_UNSIGNED, mpi_rank + 1, SETUP, MPI_COMM_WORLD);
+			MPI_Send(&l, 1, MPI_UNSIGNED, mpi_rank + 1, SETUP, MPI_COMM_WORLD);
 		}
 	}
 
-	std::cout << nth_val << std::endl;
+	std::string file_name = "ppm/mpi.ppm";
+	MPI_File file;
+	MPI_File_open(MPI_COMM_WORLD, (char *) file_name.c_str(), MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
+	MPI_Status status;
 
-	MPI_File_seek(file, offset, MPI_SEEK_SET);
-	MPI_File_write(file, &nth_val, 1, MPI_UNSIGNED, &status);
+	// MPI_File_seek(file, offset, MPI_SEEK_SET);
+	// MPI_File_write(file, &nth_val, 1, MPI_UNSIGNED, &status);
+
+	const size_t size = std::sqrt(last - 1) + 1;
+
+	MPI_Offset offset = ppm::write_header(file, 3, size, size, 1, status);
+	size_t k = eratosthene::index_to_number_23_0(o) - 1;
+	for(size_t i = 0; i < count; ++i){
+		
+		size_t j = ulam::ltos(k, size);
+		MPI_File_seek(file, offset + j * 3, MPI_SEEK_SET);
+		if(prime[i]) ppm::write(file, ppm::pixel_t(1, 1, 1), status);
+		else ppm::write(file, ppm::pixel_t(0, 0, 0), status);
+		++k;
+
+		for(size_t t = 0; t < 3; ++t){
+			j = ulam::ltos(k, size);
+			MPI_File_seek(file, offset + j * 3, MPI_SEEK_SET);
+			ppm::write(file, ppm::pixel_t(0, 0, 0), status);
+			++k;
+		}
+
+		++i;
+		if(i == count) break;
+		j = ulam::ltos(k, size);
+		MPI_File_seek(file, offset + j * 3, MPI_SEEK_SET);
+		if(prime[i]) ppm::write(file, ppm::pixel_t(1, 1, 1), status);
+		else ppm::write(file, ppm::pixel_t(0, 0, 0), status);
+		++k;
+
+		j = ulam::ltos(k, size);
+		MPI_File_seek(file, offset + j * 3, MPI_SEEK_SET);
+		ppm::write(file, ppm::pixel_t(0, 0, 0), status);
+
+		++k;
+	}
+
+
+
+
+
+
+
 	MPI_File_close(&file);
-
-	// double PI;
-
-	// if(rank > 0) MPI_Recv(&PI, 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	// else PI = 3.14159;
-
-	// printf("Hello world from process %d of %d (%f, %d)\n", rank, size, PI, last);
-
-
-	// if(rank < size - 1){
-	// 	MPI_Send(&PI, 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
-	// }
-
-
 	MPI_Finalize();
 
 	return 0;
