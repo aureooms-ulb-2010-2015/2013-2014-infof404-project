90682df8308dd8cae0aba12e41ee16ea5b1c1443
aureooms <aurelien.ooms@gmail.com>
2014-09-13 17:22:41 +0200

    up

--- 2/h/lib/bits_2_t.hpp
+++ 2/h/lib/bits_2_t.hpp
@@ -11,7 +11,7 @@ class bits_2_t{
 
 public:
 	typedef B block_t;
-	static const size_t block_size = sizeof(block_t) * 8;
+	static const int block_size = sizeof(block_t) * __CHAR_BIT__;
 
 private:
 	block_t* _data;
@@ -27,8 +27,10 @@ public:
 
 		bit_ref(block_t& block, const block_t mask) noexcept : block(&block), mask(mask){}
 
-		inline bool operator= (bool v) const noexcept{
-			return v ? *block |= mask : *block &= ~mask;
+		inline bool operator= (const bool v) const noexcept{
+			if (v) *block |= mask;
+			else   *block &= ~mask;
+			return v;
 		}
 
 		inline operator bool() const noexcept{
@@ -39,18 +41,18 @@ public:
 	bits_2_t () noexcept : _data(nullptr){}
 
 	bits_2_t (const size_t n) noexcept : _data(nullptr){
-		const block_t fill(0);
 		_size = n;
 		_blocks = number_of_blocks_needed(n);
 		_data = new block_t[_blocks];
+		const block_t fill(0);
 		std::fill(_data, _data + _blocks, fill);
 	}
 
 	bits_2_t(const size_t n, const bool v) noexcept : _data(nullptr){
-		const block_t fill = v ? ~block_t(0) : block_t(0);
 		_size = n;
 		_blocks = number_of_blocks_needed(n);
 		_data = new block_t[_blocks];
+		const block_t fill = v ? ~block_t(0) : block_t(0);
 		std::fill(_data, _data + _blocks, fill);
 	}
 
@@ -59,23 +61,16 @@ public:
 	}
 
 	void resize(const size_t n, const bool v) noexcept{
-		const block_t fill = v ? ~block_t(0) : block_t(0);
 		const size_t tmpblocks = number_of_blocks_needed(n);
 		const size_t copysize = std::min(_blocks, tmpblocks);
 		block_t* tmpdata = new block_t[tmpblocks];
 		std::copy(_data, _data + copysize, tmpdata);
+		const block_t fill = v ? ~block_t(0) : block_t(0);
 		std::fill(tmpdata + copysize, tmpdata + tmpblocks, fill);
 		delete[] _data;
 		_data = tmpdata;
 		_blocks = tmpblocks;
 		_size = n;
-	}
-
-	void clear() noexcept{
-		delete[] _data;
-		_size = 0;
-		_blocks = 0;
-		_data = nullptr;
 	}
 
 	inline size_t number_of_blocks_needed (const size_t n) const noexcept {
@@ -86,28 +81,35 @@ public:
 		return _data[i / block_size];
 	}
 	
-	inline size_t index_in_block(const size_t i) const noexcept{
-		return i % block_size;
-	}
+	// inline uint8_t index_in_block(const uint8_t i) const noexcept{
+	// 	return i % block_size;
+	// }
 
 	inline bit_ref operator[](const size_t i) noexcept{
-		return bit_ref(block_at_index(i), block_t(1) << index_in_block(i));
+		return bit_ref(block_at_index(i), block_t(1) << (i % block_size));
 	}
 
 	inline bool operator[](const size_t i) const noexcept{
-		return (bool)(block_at_index(i) & (block_t(1) << index_in_block(i)));
+		return (bool)(block_at_index(i) & (block_t(1) << (i % block_size)));
 	}
 
-	block_t* data(){
+	inline block_t* data(){
 		return _data;
 	}
 
-	const block_t* data() const{
+	inline const block_t* data() const{
 		return _data;
 	}
 
-	const size_t size() const{
+	inline size_t size() const{
 		return _size;
+	}
+
+	void clear() noexcept{
+		delete[] _data;
+		_size = 0;
+		_blocks = 0;
+		_data = nullptr;
 	}
 
 	~bits_2_t(){
